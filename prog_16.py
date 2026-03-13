448ce19cd1707583b811fdba0c48a332eb7c0c3c
angeljean6 <ajrescober@gmail.com>
2026-03-14 00:41:11 +0800

    added print statement

--- prog_16.py
+++ prog_16.py
@@ -10,3 +10,5 @@ num9 = int(input("Enter number 9: "))
 num10 = int(input("Enter number 10: "))
 
 result = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10
+
+print("The result is:", result)
