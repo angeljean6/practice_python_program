b6629b8b2db54ee9a50da66f2388c9ea997b2051
angeljean6 <ajrescober@gmail.com>
2026-03-19 19:40:48 +0800

    print statement

--- prog_23.py
+++ prog_23.py
@@ -3,3 +3,13 @@ numbers = []
 while True:
     try:
         num = int(input("Enter a number: "))
+
+        if num in numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+
+        numbers.append(num)
+    except ValueError:
+        print("Invalid input. Program stopped.")
+        break
