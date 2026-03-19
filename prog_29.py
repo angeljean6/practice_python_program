a25c40ffe6e71e033b0862b45116f998c2eb1763
angeljean6 <ajrescober@gmail.com>
2026-03-19 23:34:28 +0800

    added to fix the error

--- prog_29.py
+++ prog_29.py
@@ -4,3 +4,6 @@ while True:
     try:
         num = int(input("Enter a number: "))
         numbers.append(num)
+    except ValueError:
+        print("Invalid input. Program stopped.")
+        break
