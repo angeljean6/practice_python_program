b35f026c434303ece01ccb9aecf7ce6d6faa7d5f
angeljean6 <ajrescober@gmail.com>
2026-03-19 20:03:41 +0800

    added functions

--- prog_26.py
+++ prog_26.py
@@ -3,3 +3,7 @@ for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     numbers.append(num)
 duplicates = []
+
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
