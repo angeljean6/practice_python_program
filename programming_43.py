a6567ee1bb230eb8edf92b20e52edab48a617f41
angeljean6 <ajrescober@gmail.com>
2026-03-20 14:25:06 +0800

    added converter

--- programming_43.py
+++ programming_43.py
@@ -1,3 +1,12 @@
 text = input("Enter a string: ")
 
 result = ""
+
+for ch in text:
+    ascii_val = ord(ch)
+
+# If uppercase A-Z, convert to lowercase
+    if 65 <= ascii_val <= 90:
+        result += chr(ascii_val + 32)
+    else:
+        result += ch
