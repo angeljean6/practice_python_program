4868dd2c6a0b17cf34bf61b06471e3128c284615
angeljean6 <ajrescober@gmail.com>
2026-03-20 13:04:32 +0800

    added reverse casing and functions

--- prog_36.py
+++ prog_36.py
@@ -1,1 +1,9 @@
 fullname = input("Enter your full name: ")
+reverse_case = ""
+for ch in fullname:
+    if ch.islower():
+        reverse_case += ch.upper()
+    elif ch.isupper():
+        reverse_case += ch.lower()
+    else:
+        reverse_case += ch 
