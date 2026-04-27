2ebf93e3a5fb8ec149f2f474b26dd8fda92566b7
angeljean6 <ajrescober@gmail.com>
2026-04-28 00:59:31 +0800

    Added process numbers method

--- reads_text_file_named_numbers.py
+++ reads_text_file_named_numbers.py
@@ -4,4 +4,6 @@ class number_file_divider:
         self._input_file_path = input_file_path
         self._even_file_path = even_file_path
         self._odd_file_path = odd_file_path
+
+    def process_numbers(self):
         
