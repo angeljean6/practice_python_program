e1f8a117ccfee97c418302279f1d8f6513e9a980
angeljean6 <ajrescober@gmail.com>
2026-04-28 01:04:18 +0800

    Implement file handling using context manager

--- reads_text_file_named_numbers.py
+++ reads_text_file_named_numbers.py
@@ -6,4 +6,7 @@ class number_file_divider:
         self._odd_file_path = odd_file_path
 
     def process_numbers(self):
-        
+        with open(self._input_file_path, "r") as cosmic_input_stream, \
+             open(self._even_file_path, "w") as azure_even_stream, \
+             open(self._odd_file_path, "w") as crimson_odd_stream:
+            
