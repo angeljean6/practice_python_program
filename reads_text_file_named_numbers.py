57795edcf17202fe0569a01f36d27716345022dd
angeljean6 <ajrescober@gmail.com>
2026-04-28 00:53:29 +0800

    Added process_numbers method, added even odd and output files

--- reads_text_file_named_numbers.py
+++ reads_text_file_named_numbers.py
@@ -1,2 +1,7 @@
 class number_file_divider:
 
+    def __init__(self, input_file_path, even_file_path, odd_file_path):
+        self._input_file_path = input_file_path
+        self._even_file_path = even_file_path
+        self._odd_file_path = odd_file_path
+        
